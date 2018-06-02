ae9654937a2a54ba35d955b1df2bf299741b8758
Marcel <khouri10@gmail.com>
2018-06-01 21:51:07 -0300

    Create custom views for the administration site

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,7 @@ from .models import Order, OrderItem
 import csv
 import datetime
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse
 
 class OrderItemInline(admin.TabularInline):
 	model = OrderItem
@@ -29,12 +30,16 @@ def export_to_csv(modeladmin, request, queryset):
 			data_row.append(value)
 		writer.writerow(data_row)
 	return response
-
 export_to_csv.short_description = 'Export to CSV'
 
+def order_detail(obj):
+	return '<a href="{}">View</a>'.format( 
+		reverse('orders:admin_order_detail', args=[obj.id]))
+order_detail.allow_tags = True 
+
 class OrderAdmin(admin.ModelAdmin):
-	list_display = ['id', 'first_name', 'last_name', 'email',
-	'address', 'postal_code', 'city', 'paid', 'created', 'updated']
+	list_display = ['id', 'first_name', 'last_name', 'email', 'address', 
+		'postal_code', 'city', 'paid', 'created', 'updated', order_detail]
 	list_filter = ['paid', 'created', 'updated']
 	inlines = [OrderItemInline]
 	actions = [export_to_csv]
